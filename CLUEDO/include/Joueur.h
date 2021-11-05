7474cc64f31581724ce15452c5698a90a7376b31
AlexLees1 <91546247+AlexLees1@users.noreply.github.com>
2021-11-05 13:49:10 +0100

    Add files via upload

---
+++ CLUEDO/include/Joueur.h
@@ -0,0 +1,53 @@
+#ifndef JOUEUR_H
+#define JOUEUR_H
+
+#include "Carte.h"
+
+class Joueur
+{
+    public:
+        Joueur();
+        ~Joueur();
+
+        std::string getNom()const;
+        int getPosX()const;
+        int getPosY()const;
+        int getDeplacement()const;
+        char getLoca()const;
+        std::string getCouleurPion()const;
+        BITMAP* getImagePion()const;
+        std::map <std::string, bool> getHypo()const;
+        std::vector<Carte> getCartes()const ;
+
+        void setNom(std::string nom);
+        void setPosX(int posX);
+        void setPosY(int posY);
+        void setDeplacement(int nb_depl);
+        void setLoca(char loca);
+        void setCouleur(std::string couleur);
+        void setImagePion(BITMAP* logo);
+        void setHypo(std::string nouvinfo);
+
+
+        void add(Carte carte);
+        void cardDisplay(BITMAP* buffer)const;
+        void hypotheseInit();
+        void infoDisplay(BITMAP* buffer);
+        void hypoDisplay(BITMAP* buffer)const;
+        void deplacement(BITMAP* plateau, BITMAP* buffer, std::vector<std::vector <char>> &matrice, std::vector<Joueur> &joueurs);
+
+    private:
+        std::string m_nom ;
+        int m_posX ;
+        int m_posY ;
+        int m_deplacement;
+        char m_loca;
+        std::string m_couleurPion ;
+        BITMAP* m_imagePion ;
+
+        std::map <std::string, bool> m_hypothese ;
+        std::vector <Carte> m_cartes ;
+
+};
+
+#endif // JOUEUR_H
